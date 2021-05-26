069d3e0d7176e1d6f09a32f9ebbda0d141b3d042
ErturkErismis <erturkerismis12@outlook.com>
2021-05-26 12:38:50 +0300

    Backend App

--- CarPricePredictor.py
+++ CarPricePredictor.py
@@ -6,6 +6,19 @@ from Audi_Window import *
 from Bmw_Window import *
 from Merc_Window import *
 from Ford_Window import *
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import seaborn as sbn
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn.metrics import mean_absolute_error,mean_squared_error
+from tensorflow.keras.models import load_model
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+import joblib
 
 #----------------------Create App-------------------#
 #---------------------------------------------------------#
@@ -96,6 +109,140 @@ def Ford_Clear():
     ui5.Ford_enginesize_lineEdit.clear()
     ui5.Merc_price_lineEdit.clear()
 
+#----------------------CalculateButtons -------------------#
+#---------------------------------------------------------#
+
+def Audi_Calculate():
+
+    model=load_model("AudiModel.h5")
+    scaler=joblib.load("Audiscaler.save")
+
+    Year=float(ui2.lineEdit.text())
+    Mage=float(ui2.lineEdit_2.text())
+    tax =float(ui2.lineEdit_3.text())
+    Mpg=float(ui2.lineEdit_4.text())
+    Enginesize=float(ui2.lineEdit_5.text())
+
+    if ui2.comboBox_2.currentText() == "AUTOMATİC":
+        #Arr.append(1)
+        #Arr.append(0)
+        #Arr.append(0)
+        Arr=[[Year,Mage,tax,Mpg,Enginesize,1,0,0]]
+    elif ui2.comboBox_2.currentText() == "MANUEL":
+        #Arr.append(0)
+        #Arr.append(1)
+        #Arr.append(0)
+        Arr=[[Year,Mage,tax,Mpg,Enginesize,0,1,0]]
+    elif ui2.comboBox_2.currentText() == "SEMİ-AUTO":
+        #Arr.append(0)
+        #Arr.append(0)
+        #Arr.append(1)
+        Arr=[[Year,Mage,tax,Mpg,Enginesize,0,0,1]]
+    if ui2.comboBox_3.currentText() == "DİESEL":
+        Arr[0].append(1)
+        Arr[0].append(0)
+        Arr[0].append(0)
+    elif ui2.comboBox_3.currentText() == "PETROL":
+        Arr[0].append(0)
+        Arr[0].append(0)
+        Arr[0].append(1)
+    elif ui2.comboBox_3.currentText() == "HYBRİD":
+        Arr[0].append(0)
+        Arr[0].append(1)
+        Arr[0].append(0)
+
+    if ui2.comboBox.currentText() == "A1":
+        Arr2=[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    
+    elif ui2.comboBox.currentText() == "A2":
+        Arr2=[0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    
+    elif ui2.comboBox.currentText() == "A3":
+        Arr2=[0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    
+    elif ui2.comboBox.currentText() == "A4":
+        Arr2=[0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+    elif ui2.comboBox.currentText() == "A5":
+        Arr2=[0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+    elif ui2.comboBox.currentText() == "A6":
+        Arr2=[0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+    elif ui2.comboBox.currentText() == "A7":
+        Arr2=[0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+    elif ui2.comboBox.currentText() == "A8":
+        Arr2=[0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+    elif ui2.comboBox.currentText() == "Q2":
+        Arr2=[0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+    elif ui2.comboBox.currentText() == "Q3":
+        Arr2=[0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+    elif ui2.comboBox.currentText() == "Q5":
+        Arr2=[0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+    elif ui2.comboBox.currentText() == "Q7":
+        Arr2=[0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+    elif ui2.comboBox.currentText() == "Q8":
+        Arr2=[0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        
+    elif ui2.comboBox.currentText() == "R8":
+        Arr2=[0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0]
+
+    elif ui2.comboBox.currentText() == "RS3":
+        Arr2=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0]
+
+    elif ui2.comboBox.currentText() == "RS4":
+        Arr2=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0]
+
+    elif ui2.comboBox.currentText() == "RS5":
+        Arr2=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0]
+
+    elif ui2.comboBox.currentText() == "RS6":
+        Arr2=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0]
+
+    elif ui2.comboBox.currentText() == "RS7":
+        Arr2=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0]
+
+    elif ui2.comboBox.currentText() == "S3":
+        Arr2=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0]
+
+    elif ui2.comboBox.currentText() == "S4":
+        Arr2=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0]
+
+    elif ui2.comboBox.currentText() == "S5":
+        Arr2=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0]
+
+    elif ui2.comboBox.currentText() == "S8":
+        Arr2=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0]
+
+    elif ui2.comboBox.currentText() == "SQ5":
+        Arr2=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0]
+
+    elif ui2.comboBox.currentText() == "SQ7":
+        Arr2=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0]
+
+    elif ui2.comboBox.currentText() == "TT":
+        Arr2=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+
+
+    Arr[0].extend(Arr2)
+    
+    #for x in Arr:
+       # print(x)
+    
+    #Arr=[[2014,31930,580,21.9,5.2,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0]]
+
+    Arr=scaler.transform(Arr)
+
+    x=model.predict(Arr)
+
+    ui2.lineEdit_6.setText(str(x))
+    print(x)
 
 
 
@@ -109,6 +256,7 @@ ui.Ford_winbutton.clicked.connect(Ford_Win)
 ui.Bmw_winbutton.clicked.connect(Bmw_Win)
 ui.Merc_winbutton.clicked.connect(Merc_Win)
 ui2.pushButton_2.clicked.connect(Audi_Clear)
+ui2.pushButton.clicked.connect(Audi_Calculate)
 ui3.Bmw_Clear_pushButton.clicked.connect(Bmw_Clear)
 ui4.Merc_Clear_pushButton.clicked.connect(Merc_Clear)
 ui5.Ford_Clear_pushButton.clicked.connect(Ford_Clear)
